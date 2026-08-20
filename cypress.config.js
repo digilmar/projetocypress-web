2d289dc6cfeb3320816acf1aae6c21d1323c1f4f
Gilmar Figueiredo da Silva <digilmar@gmail.com>
2026-08-20 10:54:18 -0300

    Adicionado relatório Mochawasome

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'f2apqt',
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+     reporterOptions: {
+    charts: true,
+    reportTitle: 'Curso de Cypress',
+    reportPageTitle: 'Projeto do curso de Cypress',
+     },
     baseUrl: 'https://automationpratice.com.br/',
     defaultCommandTimeout: 5000, // Tempo limite padrão para comandos
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implemente eventos de nó aqui
     },
     viewportWidth: 1280,  // Largura global
